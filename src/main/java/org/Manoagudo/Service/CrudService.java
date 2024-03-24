2df7d483ebcbb81d7971c6d18f09862ceee385a2
Helsk8poli <164684186+Helsk8poli@users.noreply.github.com>
2024-03-24 18:00:16 -0500

    Update CrudService.java

--- src/main/java/org/Manoagudo/Service/CrudService.java
+++ src/main/java/org/Manoagudo/Service/CrudService.java
@@ -1,7 +1,14 @@
 package org.Manoagudo.Service;
 
+// Clase que proporciona métodos para operaciones CRUD 
+
 public class CrudService {
+
+// Método estático para ejecutar las operaciones CRUD
+    
     public static void runner(){
 
     }
 }
+
+// Se implementara CRUD en proximas entregas
