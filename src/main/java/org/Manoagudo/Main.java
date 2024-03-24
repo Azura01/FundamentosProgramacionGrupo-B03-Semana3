a107e1029615e1c610044542916d7940f1f8db92
Helsk8poli <164684186+Helsk8poli@users.noreply.github.com>
2024-03-24 18:11:57 -0500

    Update Main.java

--- src/main/java/org/Manoagudo/Main.java
+++ src/main/java/org/Manoagudo/Main.java
@@ -7,10 +7,18 @@ import org.Manoagudo.Service.GenerateInfoFiles;
 import java.util.ArrayList;
 import java.util.List;
 
+// Clase principal que contiene el método main para iniciar la aplicación
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Bienvenido docente!");
+        System.out.println("Bienvenido docente!"); // Mensaje de bienvenida
+        
+// Creación de una instancia de GenerateInfoFiles para generar archivos de información 
+        
         GenerateInfoFiles generarArchivos = new GenerateInfoFiles();
+
+// Llamada a los métodos para generar archivos de productos y de historial de ventas
+        
         generarArchivos.createProductList();
         generarArchivos.createRecordSells();
 
