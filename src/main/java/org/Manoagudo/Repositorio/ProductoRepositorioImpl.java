6071f32ec41a8c8adab87060927635fc637375e1
Helsk8poli <164684186+Helsk8poli@users.noreply.github.com>
2024-03-24 17:54:37 -0500

    Update ProductoRepositorioImpl.java

--- src/main/java/org/Manoagudo/Repositorio/ProductoRepositorioImpl.java
+++ src/main/java/org/Manoagudo/Repositorio/ProductoRepositorioImpl.java
@@ -5,20 +5,30 @@ import org.Manoagudo.Entidades.Producto;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ProductoRepositorioImpl implements Repositorio{
+// Implementación concreta de un repositorio para la entidad Producto
 
+public class ProductoRepositorioImpl implements Repositorio{
+    
+// Método para listar productos
+    
     @Override
     public List listar() {
+        
+// Se crea una lista de productos vacía
+        
         List<Producto>productos =new ArrayList<>();
-
+        
+// Se devuelve la lista de productos
+        
         return null;
     }
-
+    
+ // Método para guardar un objeto
     @Override
     public void guardar(Object o) {
 
     }
-
+// Método para eliminar un objeto
     @Override
     public void eliminar(Object o) {
 
