80b1651654a93c409ba054b31f269dbda735d102
Helsk8poli <164684186+Helsk8poli@users.noreply.github.com>
2024-03-24 17:57:35 -0500

    Update Repositorio.java

--- src/main/java/org/Manoagudo/Repositorio/Repositorio.java
+++ src/main/java/org/Manoagudo/Repositorio/Repositorio.java
@@ -2,9 +2,20 @@ package org.Manoagudo.Repositorio;
 
 import java.util.List;
 
+// Interfaz genérica para un repositorio
+
 public interface Repositorio <T>{
+    
+// Método para listar elementos de tipo T
+    
     List<T> listar();
+    
+// Método para guardar un elemento de tipo T
+    
     void guardar(T t);
+
+// Método para eliminar un elemento de tipo T
+    
     void eliminar(T t);
 
 }
