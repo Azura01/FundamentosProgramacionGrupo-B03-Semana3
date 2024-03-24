454b66543e31d0a82c087cbbc88001f463b6d2ab
Helsk8poli <164684186+Helsk8poli@users.noreply.github.com>
2024-03-24 18:02:22 -0500

    Update GeneradorArchivo.java

--- src/main/java/org/Manoagudo/Service/GeneradorArchivo.java
+++ src/main/java/org/Manoagudo/Service/GeneradorArchivo.java
@@ -1,8 +1,18 @@
 package org.Manoagudo.Service;
 
-public interface GeneradorArchivo {
+// Interfaz para un generador de archivos
 
+public interface GeneradorArchivo {
+    
+// Método para crear una lista de productos en un archivo
+    
     public void createProductList();
+
+// Método para crear una lista de vendedores en un archivo
+    
     public void createVendorList();
+
+// Método para crear un registro de ventas en un archivo
+    
     public void createRecordSells();
 }
