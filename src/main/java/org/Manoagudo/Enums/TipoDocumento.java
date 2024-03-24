dd8d97d440de0aa61a5a9222305452edecbcd04f
Helsk8poli <164684186+Helsk8poli@users.noreply.github.com>
2024-03-24 17:51:04 -0500

    Update TipoDocumento.java

--- src/main/java/org/Manoagudo/Enums/TipoDocumento.java
+++ src/main/java/org/Manoagudo/Enums/TipoDocumento.java
@@ -1,16 +1,24 @@
 package org.Manoagudo.Enums;
 
+// Enumeración que representa diferentes tipos de documentos
+
 public enum TipoDocumento {
     CC("Cedula de Ciudadania"),
     CE("Cedula de Extranjeria"),
     PS("Pasaporte");
-
+    
+ // Atributo que almacena la descripción del tipo de documento
+    
     private String tipoDocumento;
-
+    
+// Constructor privado para inicializar el tipo de documento
+    
     private TipoDocumento(String tipoDocumento){
         this.tipoDocumento = tipoDocumento;
     }
-
+    
+// Método para obtener la descripción del tipo de documento
+    
     public String getTipoDocumento(){
         return tipoDocumento;
     }
